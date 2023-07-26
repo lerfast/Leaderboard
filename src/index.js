a6bffffccf3c4e6b138f9edf44e519fd43fcad10
Luis Emilio Rojas <luisemiliorojas@hotmail.com>
2023-07-26 15:45:46 -0500

    Applying API to project
    Modify index.js for exercise code.
    Running Linters

--- src/index.js
+++ src/index.js
@@ -1,1 +1,65 @@
 import './index.css';
+
+const getScores = async (gameId) => {
+  const response = await fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores/`);
+  const data = await response.json();
+  return data.result;
+};
+
+const saveScore = async (gameId, userName, userScore) => {
+  const response = await fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores/`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      user: userName,
+      score: userScore,
+    }),
+  });
+
+  const data = await response.json();
+  return data.result;
+};
+
+const customGameId = 'GameByLuis'; 
+
+document.getElementById('refresh-btn').addEventListener('click', async () => {
+  const scores = await getScores(customGameId);
+
+  const scoresContainer = document.getElementById('scores');
+  scoresContainer.innerHTML = '';
+
+  scores.forEach((score) => {
+    const scoreRow = document.createElement('div');
+    scoreRow.classList.add('scores-row');
+    if (scores.indexOf(score) % 2 === 1) {
+      scoreRow.classList.add('odd-row');
+    }
+    const scoreDescription = document.createElement('p');
+    scoreDescription.classList.add('row-description');
+    scoreDescription.textContent = `${score.user}: ${score.score}`;
+    scoreRow.appendChild(scoreDescription);
+    scoresContainer.appendChild(scoreRow);
+  });
+});
+
+document.getElementById('add-form').addEventListener('submit', async (event) => {
+  event.preventDefault();
+
+  const nameInput = document.getElementById('name');
+  const scoreInput = document.getElementById('score');
+
+  const userName = nameInput.value;
+  const userScore = parseInt(scoreInput.value, 10);
+
+  if (Number.isNaN(userScore) || userScore <= 0) {
+    alert('Please enter a valid score.');
+    return;
+  }
+
+  await saveScore(customGameId, userName, userScore);
+  nameInput.value = '';
+  scoreInput.value = '';
+  console.log('Score submitted successfully!'); 
+});
